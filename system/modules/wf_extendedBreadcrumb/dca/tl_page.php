6bc06034982f23babe4a5efd96cd21da8aa586df
Andreas Isaak <andy.jared@googlemail.com>
2012-02-13 14:16:54 +0100

    Preparing for Contao 2.11

--- system/modules/wf_extendedBreadcrumb/dca/tl_page.php
+++ system/modules/wf_extendedBreadcrumb/dca/tl_page.php
@@ -30,12 +30,11 @@
 /**
  * Palettes
  */
-
-foreach($GLOBALS['TL_DCA']['tl_page']['palettes'] as $key=>$row)
+foreach ($GLOBALS['TL_DCA']['tl_page']['palettes'] as $key => $row)
 {
-    if($key == '__selector__') continue;
-	if(!stristr($row,'hide,')) continue;
-	$GLOBALS['TL_DCA']['tl_page']['palettes'][$key] = str_replace('hide,','hide,wf_extendedBreadcrumb,',$GLOBALS['TL_DCA']['tl_page']['palettes'][$key]);
+    if ($key == '__selector__') continue;
+    if (!stristr($row, 'hide,')) continue;
+    $GLOBALS['TL_DCA']['tl_page']['palettes'][$key] = str_replace('hide,', 'hide,wf_extendedBreadcrumb,', $GLOBALS['TL_DCA']['tl_page']['palettes'][$key]);
 }
 
 
@@ -43,12 +42,10 @@ foreach($GLOBALS['TL_DCA']['tl_page']['palettes'] as $key=>$row)
  * Fields
  */
 $GLOBALS['TL_DCA']['tl_page']['fields']['wf_extendedBreadcrumb'] = array
-(
-	'label'                   	=> &$GLOBALS['TL_LANG']['tl_page']['wf_extendedBreadcrumb'],
-	'exclude'                 	=> true,
-	'inputType'               	=> 'checkbox',
-	'eval' 				=> array('tl_class' => 'w50')
+    (
+    'label' => &$GLOBALS['TL_LANG']['tl_page']['wf_extendedBreadcrumb'],
+    'exclude' => true,
+    'inputType' => 'checkbox',
+    'eval' => array('tl_class' => 'w50')
 );
-
-
 ?>
